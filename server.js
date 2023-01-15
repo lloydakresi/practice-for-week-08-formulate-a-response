7ab48f737701c32fc9275c095a295bf620360623
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-15 15:35:45 +0000

    complete

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Your code here
+const http = require("http");
+
+const htmlText = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+</head>
+<body>
+  <h1>Hello there!</h1>
+</body>
+</html>`
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html charset-utf8");
+    res.end(htmlText);
+})
+
+const port = 5000;
+server.listen(port, ()=>{
+    console.log(`Server is listening to port ${port}`)
+})
